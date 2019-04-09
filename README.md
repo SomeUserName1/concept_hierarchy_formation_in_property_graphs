59f7ff92e784b3c4c73ef937a1039e59d703c5ec
Fabian Klopfer <fabian.klopfer@ieee.org>
2019-04-09 18:23:29 +0200

    refactor distance matrix to be in the nodes, restructure basic arch, add pseudo code

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# bachelor_project
+# Graph Structuring Toolbox
+
+This project is intended to provide tools that help to extract further structure 
+from graphs, especially Neo4J data sets, in order to support cardinality estimation  
+
+## 1. Pre-Processing
+Currently only data set specific loaders and an interface  
+Shall be able to sample and filter data to induce certain conditions
+(e.g. only cluster instances with a certain category as property)  
+
+
+## 2. Clustering
+Currently only multi-line properties of nodes.  
+Shall at some point discover sub-labels/"types" for Nodes, later maybe also for
+edges or sub-graphs
+
+Algorithms to implement:  
+- Linkage-based clustering (single and complete linkage) (for nodes/edges)
+- Adapted version of chameleon clustering (for sub-graphs)
+
+## 3. Sub-Graph Mining
+future work, maybe FP-Tree and Apriori-Based. See literature/GraphMining  
+Maybe DIMSpan?
+
+## 4. Visualization
+Shall provide a wrapper to call algorithm specific visualizations 
+(e.g. Dendrogram, ...)
+
+## 5. Cardinality estimation
+Leo
+
+TODO: Metrics (e.g. InfoGain, Fisher Info/Score, Shilouette coef., lift, ...)  
+
+_Falls möglich und mir langweilig: compression und type hierarchy extraction über datensätze hinweg aber jetzt programmier ich lieber sonst hab ich mit 40 noch nicht mal meinen Bachelor_
--- src/cluster/Dendrogram/DendrogramClustering.java
+++ src/cluster/Dendrogram/DendrogramClustering.java
@@ -5,55 +5,64 @@ import preprocess.YelpBusiness.YelpBusiness;
 
 import java.util.*;
 
+/**
+ * Implements hierarchical linkage-based clustering.
+ *
+ * Pseudo code:
+ *  1. Initial clusters consisting of individual nodes, calculate distances
+ *      for all pairs of clusters.
+ *  2. Merge closest clusters
+ *  3. Update distance of cluster to all others
+ *  4. If there is only one cluster left, terminate, else goto 2.
+ *
+ *  Leaf Nodes: contain a single Object of the data type -> Initial loss matrix
+ *  Inner Nodes: contain union of all children -> recalculate loss vector based
+ *      on linkage convention
+ */
 public class DendrogramClustering implements Clustering {
-    private int[][] distances;
-    private ArrayList<YelpBusiness> data;
+    private ArrayList<DendrogramNode> working_set = new ArrayList<>();
 
     public DendrogramClustering(ArrayList<YelpBusiness> b) {
-        this.data = b;
-        calculateInitialDistances();
+        for (YelpBusiness yb : b) {
+            DendrogramNode<YelpBusiness> leaf = new DendrogramLeafNode<>(yb);
+            working_set.add(leaf);
+        }
         cluster();
-        System.out.println(Arrays.deepToString(distances));
     }
 
 
 
     public void calculateInitialDistances() {
-        this.distances = new int[this.data.size()][];
+        // TODO do this per nodes and in the nodes;
+/*        this.distances = new int[this.data.size()][];
         for (int i = this.distances.length; i > 0; i--) {
             this.distances[i-1] = new int[i];
-            System.out.println("outer index: " + i);
             for (int j = 0; j < i; j++) {
                 this.distances[i-1][j] = this.data.get(i-1).compare_attribs(this.data.get(j));
             }
-        }
+        }*/
     }
 
 
     public void cluster() {
-        merge();
-        while
-    }
-
-    private int find_minimum_distance() {
-        int min_idx_i = 0;
-        int min_idx_j = 0;
-        int min = Integer.MAX_VALUE;
-
-        for (int i = 0; i < this.distances.length; i++) {
-            for (int j = 0; j < this.distances[i].length; j++) {
-                if (this.distances[i][j] < min) {
-                    min_idx_i = i;
-                    min_idx_j = j;
-                }
-            }
+        while (this.working_set.size() > 1) {
+            //merge
+            //update_distances
         }
-        return
     }
 
-    // Single Linkage Distance: D(C_i, C_j) = min_{p in C_i, q in C_j} (dist(p,q))
+    private void merge() {
+        // TODO
+    }
 
-    // Complete Linkage Distance: D(C_i, C_j) = max_{p in C_i, q in C_j} (dist(p,q))
-    
+    private void update_distance() {
+        // TODO
+    }
+
+    private List<Integer> find_minimum_distance() {
+        // TODO
+        return null;
+    }
+
 
 }
