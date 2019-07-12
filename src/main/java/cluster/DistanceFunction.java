52628319dee157ce5e697a25606d84f5573addd4
Fabian Klopfer <fabian.klopfer@ieee.org>
2019-07-12 15:27:57 +0200

    change the structure

--- src/main/java/cluster/DistanceFunction.java
+++
@@ -1,9 +0,0 @@
-package cluster;
-
-import cluster.dendrogram.DendrogramNode;
-import preprocess.DataObject;
-
-@FunctionalInterface
-public interface DistanceFunction<T extends DataObject> {
-  Float calculate(DendrogramNode<T> node1, DendrogramNode<T> node2);
-}
